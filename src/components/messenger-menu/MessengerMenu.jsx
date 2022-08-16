f02549c0b6f6e6d2af519dcd09a79184f571a88e
Sem Kholodov <kholodovsem@gmail.com>
2022-08-16 21:56:10 +0300

    Chat

--- src/components/messenger-menu/MessengerMenu.jsx
+++ src/components/messenger-menu/MessengerMenu.jsx
@@ -1,20 +1,10 @@
 import s from './MessengerMenu.module.scss';
-import { ReactComponent as UserIcon } from '../../image/user.svg';
-import { ReactComponent as LoupIcon } from '../../image/loupe.svg';
+import MenuHeader from './MenuHeader';
 
 const MessengerMenu = () => {
   return (
     <aside className={s.messengerMenu}>
-      <div className={s.menuHeader}>
-        <UserIcon className={s.menuUserIcon} />
-        <label className={s.inputLabel}>
-          <LoupIcon className={s.menuLoupIcon}/>
-          <input
-            className={s.menuInput}
-            placeholder='Search or start new chat'
-          />
-        </label>
-      </div>
+      <MenuHeader />
     </aside>
   );
 };
