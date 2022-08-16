2a45295c1f2c4cf2a69f1f03f8cc4ac990d40dec
Sem Kholodov <kholodovsem@gmail.com>
2022-08-16 21:56:00 +0300

    Message Component

--- src/components/chat/Message.jsx
+++ src/components/chat/Message.jsx
@@ -1,9 +1,16 @@
-const Message = ({message, date, time}) => {
+import s from './Messege.module.scss';
+
+const Message = ({ textMessage, date, time, src, name, myMessage }) => {
   return (
-    <p>
-      {message}
+    <div className={myMessage ? s.myMessage : null}>
+      <div className={myMessage ? null : s.messageWrapper}>
+        {myMessage ? false : <img src={src} alt={name} className={s.userPhoto} />}
+        <p className={myMessage ? s.textSecondColor : s.text}>
+          {textMessage}
+        </p>
+      </div>
       <span>{`${date}, ${time}`}</span>
-    </p>
+    </div>
   );
 };
 
