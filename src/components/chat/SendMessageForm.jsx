944d5375fd12adb5d5e2ea3cc342d6673855d801
Sem Kholodov <kholodovsem@gmail.com>
2022-08-17 17:23:05 +0300

    SendMessageForm

--- src/components/chat/SendMessageForm.jsx
+++ src/components/chat/SendMessageForm.jsx
@@ -1,12 +1,19 @@
 import s from './SendMessageForm.module.scss';
+import { ReactComponent as PaperPlane } from '../../image/paper-plane.svg';
 
-const SendMessageForm = ({handleSubmit}) => {
+const SendMessageForm = ({ handleSubmit }) => {
   return (
     <form onSubmit={handleSubmit} className={s.sendMessageForm}>
-      <input name='message' />
-      <button type='submit'>
-        Send
-      </button>
+      <label className={s.inputLabel}>
+        <input
+          name='message'
+          placeholder='Type your message'
+          className={s.sendMessageInput}
+        />
+        <button type='submit' className={s.sendMessageBtn}>
+          <PaperPlane />
+        </button>
+      </label>
     </form>
   );
 };
