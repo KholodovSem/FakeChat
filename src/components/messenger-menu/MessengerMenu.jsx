11a1c74bbe61bbceefe208f7e46483da3b9e20aa
Sem Kholodov <kholodovsem@gmail.com>
2022-08-17 17:23:55 +0300

    Update Style

--- src/components/messenger-menu/MessengerMenu.jsx
+++ src/components/messenger-menu/MessengerMenu.jsx
@@ -1,10 +1,24 @@
 import s from './MessengerMenu.module.scss';
 import MenuHeader from './MenuHeader';
+import Contact from './Contact';
+import history from '../../data/chats.json';
 
 const MessengerMenu = () => {
   return (
     <aside className={s.messengerMenu}>
       <MenuHeader />
+      <div className={s.contactsWrapper}>
+        {history.map((person) => (
+          <Contact
+            src={person.image ? person.image : null}
+            name={person.name ? person.name : null}
+            key={person.id}
+            date={person.messages.length > 0 ? person.messages[person.messages.length - 1].date : null}
+            textMessage={person.messages.length > 0 ? person.messages[person.messages.length - 1].textMessage : null}
+            id={person.id}
+          />
+        ))}
+      </div>
     </aside>
   );
 };
