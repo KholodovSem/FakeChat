3d4cd64f34cf6b225d2325ddca4662a347ba4bd2
Sem Kholodov <kholodovsem@gmail.com>
2022-08-19 00:29:22 +0300

    Update Contact.jsx

--- src/components/messenger-menu/Contact.jsx
+++ src/components/messenger-menu/Contact.jsx
@@ -3,7 +3,7 @@ import s from './Contact.module.scss';
 const Contact = ({src, name, textMessage, date, id, handleClick}) => {
   return (
     <div className={s.contactBox} onClick={() => handleClick(id)}>
-      <img src={src} className={s.personPhoto}/>
+      <img src={src} alt={name} className={s.personPhoto}/>
       <div className={s.nameMessageWrapper}>
       <span className={s.name}>{name}</span>
       <span className={s.message}>{textMessage}</span>
