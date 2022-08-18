6c9c1f86a4a0c9c4ed608513ce52e1deb9ea4618
Sem Kholodov <kholodovsem@gmail.com>
2022-08-18 13:21:49 +0300

    Init Redux Persist

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { persistor, store } from './store/store';
+import { PersistGate } from 'redux-persist/integration/react';
 import { App } from 'App';
 import './index.css';
 import 'modern-normalize/modern-normalize.css';
-import { store } from './store/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
       <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
 );
--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,24 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import { chatHistoryReducer } from './chat-history-reducer';
+import { persistReducer, persistStore, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'
+
+const middleware = [...getDefaultMiddleware({
+  serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+  }
+})]
+
+const persistConfig = {
+  key: "users",
+  storage
+}
+
+const persistedReducer = persistReducer(persistConfig, chatHistoryReducer)
 
 export const store = configureStore({
-  reducer: chatHistoryReducer
+  reducer: persistedReducer,
+  middleware
 })
+
+export const persistor = persistStore(store)
