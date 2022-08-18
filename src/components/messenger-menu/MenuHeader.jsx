3e3b645a7eff48c33e6a2fdb47802a875a217c14
Sem Kholodov <kholodovsem@gmail.com>
2022-08-18 13:22:15 +0300

    Button <Add new contact>

--- src/components/messenger-menu/MenuHeader.jsx
+++ src/components/messenger-menu/MenuHeader.jsx
@@ -2,13 +2,21 @@ import s from './MenuHeader.module.scss';
 import { ReactComponent as UserIcon } from '../../image/user.svg';
 import { ReactComponent as LoupIcon } from '../../image/loupe.svg';
 
-const MenuHeader = () => {
+const MenuHeader = ({filter, setFilter}) => {
+
+  const handleChange = (e) => {
+    setFilter(e.currentTarget.value)
+  }
+
   return (
     <div className={s.menuHeader}>
       <UserIcon className={s.menuUserIcon} />
       <label className={s.inputLabel}>
         <LoupIcon className={s.menuLoupIcon} />
         <input
+          name="filter"
+          value={filter}
+          onChange={handleChange}
           className={s.menuInput}
           placeholder='Search or start new chat'
         />
--- src/components/messenger-menu/MessengerMenu.jsx
+++ src/components/messenger-menu/MessengerMenu.jsx
@@ -1,28 +1,43 @@
 import { useSelector } from 'react-redux';
+import { useState } from 'react';
 import MenuHeader from './MenuHeader';
 import Contact from './Contact';
-
-import s from './MessengerMenu.module.scss';
 import { SliceMessage } from '../helpers/SliceMessage';
+import {ReactComponent as AddIcon} from '../../image/Add icon.svg';
+import s from './MessengerMenu.module.scss';
+
 
 const MessengerMenu = ({handleClick}) => {
-  const persons = useSelector(state => state)
+  const persons = useSelector(state => Object.values(state))
+
+  const [filter, setFilter] = useState('');
+
+  const searchPerson = () => {
+    const normalizeFilter = filter.toLowerCase().trim();
+
+    return persons.filter(({name}) => name?.toLowerCase().includes(normalizeFilter))
+  }
 
   return (
     <aside className={s.messengerMenu}>
-      <MenuHeader />
+      <MenuHeader filter={filter} setFilter={setFilter}/>
       <div className={s.contactsWrapper}>
-        {persons.map((person) => (
+        {searchPerson().length > 0 ? searchPerson().map((person) => (
           <Contact
-            src={person.image ? person.image : null}
-            name={person.name ? person.name : null}
-            key={person.id}
-            date={person.messages.length > 0 ? person.messages[person.messages.length - 1].date : null}
-            textMessage={person.messages.length > 0 ? SliceMessage(person.messages[person.messages.length - 1].textMessage) : null}
-            id={person.id}
-            handleClick={handleClick}
+          src={person.image ? person.image : null}
+          name={person.name ? person.name : null}
+          key={person.id}
+          date={person.messages?.length > 0 ? person.messages[person.messages.length - 1].date : null}
+          textMessage={person.messages?.length > 0 ? SliceMessage(person.messages[person.messages.length - 1].textMessage) : null}
+          id={person.id}
+          handleClick={handleClick}
           />
-        ))}
+          ))
+          :
+          <button className={s.addBtn}>
+            <AddIcon className={s.addIcon}/>
+            Add new contact
+          </button>}
       </div>
     </aside>
   );
